d6e6dcfdd839c8d03dcbae71caba14e51681c7a1
Jayc <116532978+JaydenChung@users.noreply.github.com>
2024-10-16 18:03:15 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "Draw-Me-Out";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+//create a canvas element
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+
+canvas.classList.add("styled-canvas");
+
+
+app.appendChild(canvas);
+
